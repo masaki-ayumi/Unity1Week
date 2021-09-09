2852878684af996131f92565f2060152aef97f13
DESKTOP-ROER1NG\まっちゃん <ilovegames.ilikesplatoon@gmail.com>
2021-09-10 00:35:19 +0900

    プレイヤーに追従するカメラスクリプト

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;
+    public Transform playerPosition;
+    public Transform cameraPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPosition = player.GetComponent<Transform>();
+
+        cameraPosition = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
