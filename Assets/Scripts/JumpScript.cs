8d65b3acc4460a046b381cc310a02066c0e5bcb0
DESKTOP-ROER1NG\まっちゃん <ilovegames.ilikesplatoon@gmail.com>
2021-09-10 00:36:15 +0900

    プレイヤーのジャンプ実装

---
+++ Assets/Scripts/JumpScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+    //private bool isJump = false;
+    private Rigidbody rb;   //rigidbody
+
+    [SerializeField]
+    public int jumpPower = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+    }
+
+    void Jump()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(transform.up * jumpPower, ForceMode.Impulse);
+            rb.AddForce(Vector3.right, ForceMode.Impulse);
+        }
+    }
+}
